d166648ba9073aeb22796dea2638e11569a9e474
Andrew Gray <Andrew.rhysgray@hotmail.co.uk>
2020-01-21 15:59:12 +0000

    sending to hanna

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,38 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <filmList msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import filmList  from "./components/filmList.vue";
 
 export default {
-  name: 'app',
+  name: "app",
   data() {
     return {
-Films: [],
-selectedFilms
-    }
-  }
+      Films: [],
+      selectedFilms
+    };
+  },
   components: {
-    HelloWorld
+    
+  },
+
+  mounted() {
+    fetch("https://ghibliapi.herokuapp.com/")
+      .then(response => response.json())
+      .then(data => (this.data = data));
   }
-}
+};
 </script>
 
-// mounted() {
-// fetch("https://ghibliapi.herokuapp.com/")
-// .then(response => response.json())
-// .then(data =>(this.data = data));
-// }
+
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
---
+++ src/components/filmList.vue
@@ -0,0 +1,30 @@
+
+
+// I am using this component to test the page being able to load
+
+<template>
+  <p>hello</p>
+</template>
+
+<script>
+export default {
+name: 'film-list',
+props: [films],
+componenets: {
+    "list-film": listFilm
+},
+methods: {
+
+},
+
+mounted() {
+    fetch("https://api.punkapi.com/v2/beers")
+      .then(beersData => beersData.json())
+      .then(beers => (this.beers = beers));
+
+}
+</script>
+
+<style>
+
+</style>
